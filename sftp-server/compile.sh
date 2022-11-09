62c603110ab32be8acb78a090044841c041214e7
Zongze Tang <tangbe9@gmail.com>
2022-11-09 05:36:06 +0000

    rename submodule

---
+++ sftp-server/compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+git submodule update --init
+
+
+(
+cd openssh-portable
+
+export CC="musl-gcc -static -pipe -nostdinc -D_GNU_SOURCE -D_BSD_SOURCE -DHAVE_GETLINE -DHAVE_BROKEN_CHACHA20"
+
+autoreconf \
+  && ./configure \
+    --without-openssl \
+    --without-zlib \
+    --without-pam \
+    --without-xauth \
+  && make sftp-server \
+  && cp sftp-server ..
+)
